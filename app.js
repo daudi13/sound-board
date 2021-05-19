92653116dca10e3fe8ee93ae0de95940fcfea7fd
daudi13 <davouma13@gmail.com>
2021-05-19 06:14:04 +0300

    commit Js update

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 sounds.forEach(sound => {
     const btn = document.createElement('button');
     btn.classList.add('btn');
-
-    btn.innerText = sound
+    btn.innerText = sound;
+    
+    btn.addEventListener('click', () => {
+        stopAudio();
+        document.getElementById(sound).play()
+    })
     document.getElementById('buttons').appendChild(btn)
-})
+});
+
+function stopAudio() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0
+    })
+}
